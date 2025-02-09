a2da6ceab4f1b54ddcd32774d282248b74ca3771
Marina.S. <marina.sevostian@gmail.com>
2025-02-09 03:23:26 +0200

    add routes app bar and layout components

---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,19 @@
+import { Navigation } from '../Navigation/Navigation';
+import { UserMenu } from '../UserMenu/UserMenu';
+import { useSelector } from 'react-redux';
+import { AuthNav } from '../AuthNav/AuthNav';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
+import s from './AppBar.module.css';
+
+const AppBar = () => {
+  const isLogin = useSelector(selectIsLoggedIn);
+
+  return (
+    <nav className={s.navBox}>
+      <Navigation />
+      {isLogin ? <UserMenu /> : <AuthNav />}
+    </nav>
+  );
+};
+
+export default AppBar;
